6fb919719552b94ed57d4d522aed388aefc31bd4
ryanalco <ryanac11@gmail.com>
2019-07-31 11:45:12 -0700

    Tv is not included in persons project list

--- ProjectClassSol.py
+++ ProjectClassSol.py
@@ -13,31 +13,42 @@ import requests
 
 class project(object):
     
-    def __init__(self, title, cut = 0):
-        self.title = title
+    ''' Initializes a project object from the project title, which contains various pieces of info about a project(movie/tv)
+    that a person has been involved in. A rating of the project is gathered in order to filter
+    the number of projects returned for a person. The cast of the project is also gathered with a
+    'cut' value to determine which portion of the acting cast to include in the network. Various 
+    id's are included with different calls to the TMDB API in order to find all of this info based 
+    off of only the title of a project
+    '''
+    
+    def __init__(self, idnum, cut = 0):
+        self.id = idnum
+        #self.title = title
         self.cut = cut
-        self.multi_url = 'https://api.themoviedb.org/3/search/multi?api_key=e448a896945245426e4dece19f7aeca8&query='+self.title
-        self.find_id()
-        self.multi_type
-        self.id
+        #self.multi_url = 'https://api.themoviedb.org/3/search/multi?api_key=e448a896945245426e4dece19f7aeca8&query='+self.title
+        #self.find_id()
+        self.multi_type = 'movie'
+        
         self.talent = {}
         self.rating = 0
         self.find_rating()
         self.find_cast()
-        
+    '''    
     def find_id(self):
         payload = "{}"
         response = requests.request("GET", self.multi_url, data=payload)
         data = response.json()
         self.id = str(data['results'][0]['id'])
         self.multi_type = data['results'][0]['media_type']
+    '''
     
     def find_cast(self):
+        
         if self.multi_type == 'movie':
             cast_url = 'https://api.themoviedb.org/3/movie/'+self.id+'/credits?api_key=e448a896945245426e4dece19f7aeca8'
         if self.multi_type == 'tv':
             cast_url = 'https://api.themoviedb.org/3/tv/'+self.id+'/credits?api_key=e448a896945245426e4dece19f7aeca8'
-        
+        #keep this in case find way to incorporate tv
         payload = "{}"
         response = requests.request("GET", cast_url, data=payload)
         data = response.json()
